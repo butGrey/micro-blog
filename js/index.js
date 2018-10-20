4def81099423e495da7b303241a3b3ca218f84a0
butGrey <bllchestnut@163.com>
2018-10-20 13:13:39 +0800

    micro-blog

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// var oMe = document.getElementById('menu');
+// var oBox = document.getElementById('box');
+// var oMe2 = document.getElementById('menu2');
+
+// oMe.onclick = function() {
+// 	oBox.className = 'box open';
+// }
+// oMe2.onclick = function() {
+// 	oBox.className = 'box';
+// }
